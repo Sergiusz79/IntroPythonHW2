77293d9df3bbbe96f8dc2b6b188d33a4e64137c7
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-06-30 07:56:01 +0200

    014

--- Task014.py
+++ Task014.py
@@ -4,3 +4,24 @@
 (т.е. числа вида 2k), не превосходящие числа N.
 '''
 
+def enter_num():
+    try:
+        a = int(input(f"Enter number: \n"))
+    except:
+        print("Error! This is not integer number!")
+    return a
+
+
+def task():
+    n = enter_num()
+    pow = 0
+    res = 0
+    while res < n:
+        res = 2 ** pow
+        pow += 1
+        if res > n:
+            res = 2 ** (pow - 1)
+            break
+        print(res, end = ' ')
+
+task()
