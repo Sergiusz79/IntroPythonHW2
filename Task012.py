cd8b9034bcd210e86aa91056991967a6d42175b4
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-06-30 07:21:19 +0200

    012

--- Task012.py
+++ Task012.py
@@ -6,3 +6,26 @@
 Помогите Кате отгадать задуманные Петей числа.
 '''
 
+
+def enter_num(text):
+    try:
+        n = int(input(f"Enter {text} numbers: \n"))
+    except:
+        print("Error! This is not integer number!")
+    return n
+
+def two_numbers(prod, sum):
+    for i in range(1, 1000):
+        for j in range(1, 1000):
+            if i * j == prod and i + j == sum:
+                print(f'Numbers conceived by Petya is {i} and {j}')
+                return
+     
+                   
+
+def task():
+    prod = enter_num("the product of ")
+    sum = enter_num("the sum of numbers")
+    two_numbers(prod, sum)
+
+task()    
